274c143204022a09dbab944b7a166b8a0238e8f0
cognicap <it-is@cognicap.com>
2012-08-14 21:08:13 +0000

    Start your engine!

---
+++ src/org/cognicap/mobile/MainActivity.java
@@ -0,0 +1,35 @@
+package org.cognicap.mobile;
+
+
+
+import android.os.Bundle;
+
+
+
+import android.view.Menu;
+
+
+
+import org.apache.cordova.*;
+import org.cognicap.mobile.R;
+
+
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+  
+        super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash); 
+        super.loadUrl("file:///android_asset/www/index.html",4000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_hello_cordova, menu);
+        return true;
+    }
+
+    
+}
